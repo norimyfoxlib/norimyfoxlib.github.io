1bc9c8719b045b2883d62e8516ac64a58943dcfe
norimyfoxlib <66425394+norimyfoxlib@users.noreply.github.com>
2020-06-04 17:17:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,6 +53,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		onColorChange(e) {
 			const body = document.querySelector('body');
 			document.body.style.backgroundImage = 'none';
+			body.style.backgroundColor = e.target.value;
 			if (body.style.backgroundColor = `rgb(255, 255, 255`) {
 				document.body.style.background = `url(/img/123.png)`;
 			} else {
